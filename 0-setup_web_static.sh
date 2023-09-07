4f6d3ec85c761c530755d2f8652dd0fe9458706d
michaelmensa <michaelmensa@live.com>
2023-09-07 15:25:33 +0000

    task 0 web_deploy updated 1.0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,10 +15,8 @@ sudo service nginx start
 
 # create directories
 sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test
 
 #create index.html in /PATH/
 sudo touch /data/web_static/releases/test/index.html
@@ -33,12 +31,15 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 #create symbolic link to /PATH
-sudo ln -s -f /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 #change owner
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location/hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i "s|server_name _;|server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n|" /etc/nginx/sites-available/default
+
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+#sudo sed -i '/listen 80 default_server/a location/hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 #restart nginx
 sudo service nginx restart
