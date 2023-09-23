6dd5fad50c9bb25c683d691f9f13c78b09b3a328
michaelmensa <michaelmensa@live.com>
2023-09-23 23:13:49 +0000

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+''' a script that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    ''' function that returns "Hello HBNB" '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    ''' function that returns "HBNB" '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    ''' functions that handles variable '''
+    if '_' in text:
+        new_text = text.replace('_', ' ')
+        return f'C {new_text}'
+    else:
+        return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text='is cool'):
+    ''' functions that handles variable '''
+    if '_' in text:
+        new_text = text.replace('_', ' ')
+        return f'Python {new_text}'
+    else:
+        return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_int(n):
+    ''' functions that handles variable '''
+    if isinstance(n, int):
+        return f'{n} is an integer'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
